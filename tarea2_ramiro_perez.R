129dc9f6835c6c1a21fc2c079e01fa9234dc5a0d
RAMIRO <pfcperez@gmail.com>
2019-07-05 16:07:51 -0500

    Tarea 2 modificada

--- tarea2_ramiro_perez.R
+++ tarea2_ramiro_perez.R
@@ -5,25 +5,40 @@ std.dv = (sqrt(1/l^2))
 #probabilidad de que x<1.3
 x5= 1.3
 dexp(x5, l)
+pexp(x5,l)
 #probabilidad de que 1.<x<8
 x2= 1
 x3=8
 dexp(x2,l)-dexp(x3,l)
+pexp(x3,l)-pexp(x2,l)
 
 #probabilidad de que 
 x4=4
 dexp(x4,l)
+pexp(x4,l)
+
 
 par(mfrow=c(2,2))
-x= dexp(x5, l)
-x
-curve(dexp(x,l),0,10,type="l",main="Dexp x=1.3")
+x=1.3
+dexp(x,l)
+curve(dexp(x,l),-10,10,type="l",xlab="k",ylab="P(X=k)",main="Funci?n de Probabilidad N(m=1.67,sd=0.15)")
+#curve(dbinom(0:12,12,0.52),main="Densidad N(m=1.67,sd=0.15)")
+curve(pexp(x,l),-4,4,main="FDA de N(m=1.67,sd=0.15)")
+curve(qnorm(x,m,sd),main="Funci?n cuantil N(m=1.67,sd=0.15)")
 
-x= dexp(x2,l)-dexp(x3,l)
-x
-curve(dexp(x,l),0,10,main="1.<x<8")
 
-x=dexp(x4,l)
-x
-curve(dexp(x,l),0,10,main="x>4")
+
+
+#par(mfrow=c(2,2))
+#x= dexp(x5, l)
+#x
+#curve(dexp(x,l),0,10,type="l",main="Dexp x=1.3")
+
+#x= dexp(x2,l)-dexp(x3,l)
+#x
+#curve(dexp(x,l),0,10,main="1.<x<8")
+
+#x=dexp(x4,l)
+#x
+#curve(dexp(x,l),0,10,main="x>4")
 
